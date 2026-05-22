409350cf6f09e9fdb6b91f68cd76bb343b75bb67
CheiJesus09 <habichuelas03@gmail.com>
2026-05-21 20:38:44 -0600

    se agrego Gun y Scrips

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Transform gunPosition;
+
+    private void OnTriggerEnter(Collider other)
+
+    {
+        if (other.CompareTag("Gun"))
+        {
+            other.GetComponent<Gun>().GrabGun(gunPosition);
+        }
+    }
+    
+}
