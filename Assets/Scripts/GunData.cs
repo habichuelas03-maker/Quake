314d73f3bde95b3229bf346b6ddf0d85fcdde794
CheiJesus09 <habichuelas03@gmail.com>
2026-05-26 20:36:58 -0600

    se añadio bala e interfaz de scope

---
+++ Assets/Scripts/GunData.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GunData", menuName = "Scriptable Objects/GunData")]
+public class GunData : ScriptableObject
+{
+    public float demage;
+    public float fireRate;
+    public int totalBullets;
+    public float reloadTime;
+    public int cartidgeSize;
+    public GunType gunType;
+}
+
+public enum GunType
+{
+    Automatic,
+
+    SemiAutomatic,
+        
+}
